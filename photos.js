745ee8e27cae22ca309bbc665ac2ead86a8caeb9
Miguel Burgos <mr.burgos.miguel@gmail.com>
2026-07-31 09:36:17 -0500

    Update ShoreEx guide photos

--- photos.js
+++ photos.js
@@ -10,8 +10,8 @@ window.PHOTOS = {
     "zikil-paak-tortillas": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg"],
   },
   "native-kitchen": {
-    "cook-it-yourself": [],
-    "cross-the-island": [],
+    "cook-it-yourself": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
+    "cross-the-island": ["1.jpg","2.jpg"],
     "hero": [],
     "jungle-to-table-lunch": [],
     "scenic-start": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"],
